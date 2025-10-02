37cf270a17558a5d0be9260190131083ef0c07bf
igdev <dorultanianos@gmail.com>
2025-10-02 21:10:23 +0300

    feat: implement student create, update, list, get detail and update status methods

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -1,29 +1,131 @@
 const asyncHandler = require("express-async-handler");
-const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
+const {
+    getAllStudents,
+    addNewStudent,
+    getStudentDetail,
+    setStudentStatus,
+    updateStudent,
+    countAllStudents
+} = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    const {
+        page = 1,
+        limit = 10,
+        search,
+        class: className,
+        section
+    } = req.query;
+    const payload = {className, section, limit, page, search};
+    const students = await getAllStudents(payload);
+    const totalRecords = await countAllStudents(payload);
+    res.json({ page, limit, pagesCount: Math.ceil(totalRecords / limit), data: students});
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
+    const {
+        name, gender, phone,
+        email,
+        class: class_name,
+        section: section_name,
+        roll,
+        currentAddress: current_address,
+        permanentAddress: permanent_address,
+        fatherName: father_name,
+        fatherPhone: father_phone,
+        motherName: mother_name,
+        motherPhone: mother_phone,
+        guardianName: guardian_name,
+        guardianPhone: guardian_phone,
+        relationOfGuardian: relation_of_guardian,
+        systemAccess: system_access,
+        dob,
+        admissionDate: admission_dt,
+    } = req.body;
 
+    const payload = {
+        name,
+        email,
+        is_active: false,
+        phone,
+        gender,
+        dob,
+        class: class_name,
+        section: section_name,
+        roll,
+        father_name,
+        father_phone,
+        mother_name,
+        mother_phone,
+        guardian_name,
+        guardian_phone,
+        relation_of_guardian,
+        current_address,
+        permanent_address,
+        admission_dt,
+    }
+    const message = await addNewStudent(payload);
+    res.json(message);
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
+    const {id} = req.params;
+    const {
+        name, gender, phone,
+        email,
+        class: class_name,
+        section: section_name,
+        roll,
+        currentAddress: current_address,
+        permanentAddress: permanent_address,
+        fatherName: father_name,
+        fatherPhone: father_phone,
+        motherName: mother_name,
+        motherPhone: mother_phone,
+        guardianName: guardian_name,
+        guardianPhone: guardian_phone,
+        relationOfGuardian: relation_of_guardian,
+        dob,
+        admissionDate: admission_dt,
+    } = req.body;
+    const payload = {
+        userId: id,
+        name,
+        email,
+        is_active: false,
+        phone,
+        gender,
+        dob,
+        class: class_name,
+        section: section_name,
+        roll,
+        father_name,
+        father_phone,
+        mother_name,
+        mother_phone,
+        guardian_name,
+        guardian_phone,
+        relation_of_guardian,
+        current_address,
+        permanent_address,
+        admission_dt,
+    }
+    const message = await updateStudent(payload);
+    res.json(message);
 
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+    const {id} = req.params;
+    const detail = await getStudentDetail(id);
+    res.json(detail);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+    const {id: userId} = req.params;
+    const {reviewerId, status} = req.body;
+    const message = await setStudentStatus({userId, reviewerId, status});
+    res.json(message);
 });
 
 module.exports = {
--- backend/src/modules/students/students-service.js
+++ backend/src/modules/students/students-service.js
@@ -1,6 +1,11 @@
-const { ApiError, sendAccountVerificationEmail } = require("../../utils");
-const { findAllStudents, findStudentDetail, findStudentToSetStatus, addOrUpdateStudent } = require("./students-repository");
-const { findUserById } = require("../../shared/repository");
+const {ApiError, sendAccountVerificationEmail} = require("../../utils");
+const {
+    findAllStudents,
+    findStudentDetail,
+    findStudentToSetStatus,
+    addOrUpdateStudent, countStudents
+} = require("./students-repository");
+const {findUserById} = require("../../shared/repository");
 
 const checkStudentId = async (id) => {
     const isStudentFound = await findUserById(id);
@@ -16,6 +21,15 @@ const getAllStudents = async (payload) => {
     }
 
     return students;
+}
+
+const countAllStudents = async (payload) => {
+    try {
+        const [{total}] = await countStudents(payload);
+        return total;
+    } catch (error) {
+        throw new ApiError(500, "Wasn't able to count students");
+    }
 }
 
 const getStudentDetail = async (id) => {
@@ -39,34 +53,34 @@ const addNewStudent = async (payload) => {
         }
 
         try {
-            await sendAccountVerificationEmail({ userId: result.userId, userEmail: payload.email });
-            return { message: ADD_STUDENT_AND_EMAIL_SEND_SUCCESS };
+            await sendAccountVerificationEmail({userId: result.userId, userEmail: payload.email});
+            return {message: ADD_STUDENT_AND_EMAIL_SEND_SUCCESS};
         } catch (error) {
-            return { message: ADD_STUDENT_AND_BUT_EMAIL_SEND_FAIL }
+            return {message: ADD_STUDENT_AND_BUT_EMAIL_SEND_FAIL}
         }
     } catch (error) {
-        throw new ApiError(500, "Unable to add student");
+        throw new ApiError(500, `Unable to add student: ${error}`);
     }
 }
 
 const updateStudent = async (payload) => {
     const result = await addOrUpdateStudent(payload);
     if (!result.status) {
-        throw new ApiError(500, result.message);
+        throw new ApiError(500, JSON.stringify(result));
     }
 
-    return { message: result.message };
+    return {message: result.message};
 }
 
-const setStudentStatus = async ({ userId, reviewerId, status }) => {
+const setStudentStatus = async ({userId, reviewerId, status}) => {
     await checkStudentId(userId);
 
-    const affectedRow = await findStudentToSetStatus({ userId, reviewerId, status });
+    const affectedRow = await findStudentToSetStatus({userId, reviewerId, status});
     if (affectedRow <= 0) {
         throw new ApiError(500, "Unable to disable student");
     }
 
-    return { message: "Student status changed successfully" };
+    return {message: "Student status changed successfully"};
 }
 
 module.exports = {
@@ -75,4 +89,5 @@ module.exports = {
     addNewStudent,
     setStudentStatus,
     updateStudent,
+    countAllStudents,
 };
